f5506ca3d5937d8c4db53ff2b79304bb0e3f6a02
Scott Klein <scootklein@gmail.com>
2012-01-31 18:27:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WasitupHeroku::Application.config.secret_token = '3efcf2354ac02c8a925ffa0820990ac675c888fb8144368232467c7521df54b53aee64d17d35f061ea7c6226ad8302114c9b4474e962898df3f656d4213bfadf'
